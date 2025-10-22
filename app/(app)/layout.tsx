45ec340efe52d505cc26883f50936ab57fe1620c
ryantido34 <ryantido34@gmail.com>
2025-10-22 22:28:52 +0100

    chore(footer):add footer component to the app layout & update footer constants links

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from "next";
-import { Poppins, Inter } from "next/font/google";
-import "../globals.css";
 import { WhatsAppButton } from "@/components/ui/button/WhatsAppButton";
 import { Navbar } from "@/components/ui/nav/NavBar";
+import { Poppins, Inter } from "next/font/google";
+import { Footer } from "@/components/ui/Footer";
+import type { Metadata } from "next";
+import "../globals.css";
 
 const poppins = Poppins({
   weight: ["400", "500", "600"],
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body className={`${poppins.variable} ${inter.variable} antialiased`}>
         <Navbar />
         {children}
+        <Footer />
         <WhatsAppButton />
       </body>
     </html>
