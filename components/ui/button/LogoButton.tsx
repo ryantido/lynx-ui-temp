f62bd5e61f70fe7e8a140a0b1915e871041b044c
ryantido34 <ryantido34@gmail.com>
2025-10-22 22:27:01 +0100

    chore(c-ui/button):update LogoButton component ui logic for more flexibility

--- components/ui/button/LogoButton.tsx
+++ components/ui/button/LogoButton.tsx
@@ -1,19 +1,30 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-export const LogoButton = () => {
+export const LogoButton = ({
+  className = "",
+  style = "",
+}: {
+  className?: string;
+  style?: string;
+}) => {
   return (
     <Link
       href="/"
-      className="flex items-center gap-2 group"
+      className={cn("flex items-center gap-2 group", className)}
       aria-label="Lynx AI - Accueil"
     >
       <div className="w-10 h-10 rounded flex items-center justify-center relative overflow-hidden">
         <Image src="/placeholder.jpg" alt="Lynx AI Logo" fill />
       </div>
       <span
-        className="text-brand-black transition-colors font-poppins group-hover:text-brand-indigo 
-        max-md:font-medium font-bold text-lg md:text-xl lg:text-2xl"
+        className={cn(
+          "group-hover:text-brand-indigo max-md:font-medium",
+          "text-brand-black transition-colors font-poppins",
+          "font-bold text-lg md:text-xl lg:text-2xl",
+          style,
+        )}
       >
         Lynx AI
       </span>
